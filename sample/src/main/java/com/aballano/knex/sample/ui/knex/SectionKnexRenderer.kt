52b7b992c94771fd5ca01a05ea7950d6fa963cf4
Alberto Ballano <alberto.ballano@xing.com>
2019-05-02 22:34:17 +0200

    Animation sample

--- sample/src/main/java/com/aballano/knex/sample/ui/knex/SectionKnexRenderer.kt
+++ sample/src/main/java/com/aballano/knex/sample/ui/knex/SectionKnexRenderer.kt
@@ -1,5 +1,8 @@
 package com.aballano.knex.sample.ui.knex
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,5 +21,36 @@ open class SectionKnexRenderer : KnexRenderer<KnexContent<String>>() {
 
     override fun render(content: KnexContent<String>, position: Int, payloads: List<*>) {
         title.text = content.item
+    }
+
+    private val titleAnimator by lazy {
+        ObjectAnimator.ofFloat(title, "translationX", 100f).apply {
+            duration = 1000
+            repeatMode = ValueAnimator.REVERSE
+            repeatCount = ValueAnimator.INFINITE
+            setAutoCancel(true)
+            logWithData("animation ${hashCode()} created")
+        }
+    }
+
+    override fun onAttached() {
+        super.onAttached()
+        title.tag = titleAnimator.apply {
+            logWithData("animation ${hashCode()} started in onAttached")
+            start()
+        }
+    }
+
+    override fun onDetached() {
+        (title.tag as ObjectAnimator).apply {
+            logWithData("animation ${hashCode()} stopped")
+            cancel()
+        }
+        title.tag = null
+        super.onDetached()
+    }
+
+    private fun logWithData(msg: String) {
+        Log.d("DEBUG", "${hashCode()} view ${title.hashCode()} $msg")
     }
 }
